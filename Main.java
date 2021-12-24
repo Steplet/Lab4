59ab581cdfef243144bd890b6e8ed63895a473a0
Steplet <79269860+Steplet@users.noreply.github.com>
2021-12-24 14:25:09 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,71 +1,83 @@
+package others;
 import objects.*;
-import persons.*;
-import others.*;
+import persons.Persons;
 
-/**
- * Когда лодка врезалась в камыши под деревьями, на небе взошла полная луна.
- * Он немного подождал, но поскольку Снусмумрик ничего больше не добавил,
- * Муми-тролль вылез из лодки и зашагал берегом назад.
- * Снусмумрик сел на корму и осторожно выбил пепел из трубки, он наклонился и выглянул из-за камышей.
- * Хемуль уверенно держал курс вперед. Он был отчетливо виден на лунной дорожке.
- * Снусмумрик тихонько рассмеялся и начал набивать свою трубку.
- **/
+import java.util.Scanner;
 
+public class Ash extends Objects {
+    private int number = 0;
 
-public class Main {
+    public Ash(String name) {
+        super(name);
+    }
 
-    public static void main(String[] args) {
+    public class PipeException extends Exception {
+    }
 
-            //создаем персонажей
-        Persons Snufkin = new Persons("Снусмумрик");
-        Persons MoomyTroll = new Persons("Муми-тролль");
-        Persons Hemul = new Persons("Хемуль");
-            //создаем места
-        Place Coast = new Place("Берег");
-        Place OnSky = new Place("На небе");
-        Place MoonPath = new Place("Лунная дорожка");
-        Place UnderTrees = new Place("Под деревьями");
-            //создаем объекты
-        Objects Boat = new Objects("Лодка", Coast);
-        Objects Moon = new Objects("Полная луна", OnSky);
-        Objects Stern = new Objects("Корма");
-        Objects Ash = new Objects("Пепел");
-        Objects Pipe = new Objects("Трубка");
-        Objects Canes = new Objects("Камыши", UnderTrees);
+    public void put() {
+        System.out.println('\n' + "Сколько грамм пепла положить в трубку?");
+        Scanner scanner = new Scanner(System.in);
 
-        //лямбда-выражение, возвращает " но "
-        Text textBut;
-        textBut = () -> ", но ";
-
-        //лямбда-выражение, возвращает точку
-        Text textDot;
-        textDot = () -> ".";
-
-        //лямбда-выражение, возвращает запятую
-        Text textComma;
-        textComma = () -> ", ";
-
-        //лямбда-выражение, возвращает " и "
-        Text textAnd;
-        textAnd = () -> " и ";
+        try {
+            int x = Integer.parseInt(scanner.next());
+            if (x == 0) {
+                System.out.println("Трубка осталась пустой.");
+            } else if (x < 0) {
+                try {
+                    throw new PipeException();
+                } catch (PipeException exception) {
+                    System.out.println("Граммы не могут быть отрицательными.");
+                    this.put();
+                }
+            } else if (x > 50) {
+                try {
+                    throw new PipeException();
+                } catch (PipeException exception) {
+                    System.out.println("В трубке не может столько поместиться!");
+                    this.put();
+                }
+            } else {
+                number = x;
+                System.out.println("В трубке стало " + number + " грамм пепла.");
+            }
+        } catch (NumberFormatException exception) {
+            System.out.println("Введите число.");
+            this.put();
+        }
+    }
 
 
-        System.out.print(Boat.WhenBump(Canes) + textComma.addText());
-        System.out.println(Moon.Rise() + textDot.addText());
-        System.out.print(MoomyTroll.waitForAWhile() + textBut.addText() + Snufkin.addNothing() + textComma.addText() + MoomyTroll.getOut(Boat));
-        System.out.println(textAnd.addText() + MoomyTroll.walk() + textDot.addText());
-        System.out.print(Snufkin.sitOn(Stern) + textAnd.addText());
-            Snufkin.setFeel(Feeling.CAREFUL);
-        System.out.print(Snufkin.strike(Ash, Pipe) + textComma.addText());
-        System.out.println(Snufkin.lean() + textAnd.addText() + Snufkin.lookOut(Canes) + textDot.addText());
-            Hemul.setFeel(Feeling.CONFIDENT);
-        System.out.println(Hemul.cape() + textDot.addText());
-        System.out.println(Hemul.beSeen(MoonPath) + textDot.addText());
-            Snufkin.setFeel(Feeling.CALM);
-        System.out.println(Snufkin.laugh() + textAnd.addText() +Snufkin.startedFilling(Pipe) + textDot.addText());
+    public class NoAshException extends RuntimeException {
+        public NoAshException(String message) {
+            super(message);
+        }
+    }
 
-        Ash ash = new Ash("Пепел");
-        ash.put();
-        ash.smoke(Snufkin);
+    public void smoke(Persons persons) {
+        if (number == 0) {
+
+            System.out.println('\n' + "Но все-таки хочется закурить. Сколько положем пепла?");
+            Scanner scanner = new Scanner(System.in);
+            String y = scanner.next();
+
+            try {
+                if (y.equals(0)) {
+                    throw new NoAshException("Сегодня день табака, закурить обязательно!");
+                } else {
+                    for (int i = 0; i < y.length(); i++) {
+                        if ((y.charAt(i) >= 'a' && y.charAt(i) <= 'z') || (y.charAt(i) >= 'A' && y.charAt(i) <= 'Z') || (y.charAt(i) >= 'а' && y.charAt(i) <= 'я') || (y.charAt(i) >= 'А' && y.charAt(i) <= 'Я')) {
+                            throw new NoAshException("Ответ не должен содержать буквы.");
+                        } else {
+                            System.out.println(persons.getName() + " затянулся.");
+                        }
+                    }
+                }
+            } catch (NoAshException exception) {
+                System.out.println(exception.getMessage());
+            }
+        }
+        else {
+            System.out.println(persons.getName() + " смачно закурил.");
+        }
     }
 }
